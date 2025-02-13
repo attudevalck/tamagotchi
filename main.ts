9da94a259dbb492f68757d57eab96b0f56dc3e0b
attudevalck <maximknockaert@gmail.com>
2025-02-13 14:15:41 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,70 @@
-let knuffel = 20
-let voeding = 20
+input.onButtonPressed(Button.AB, function () {
+    knuffel = 20
+    voeding = 20
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+let voeding = 0
+let knuffel = 0
+knuffel = 20
+voeding = 20
 let dood = 0
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
+loops.everyInterval(1000, function () {
+    knuffel += -1
+    voeding += -1
+})
 basic.forever(function () {
-    knuffel += 5
+    if ((knuffel || voeding) < 10) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    }
+})
+basic.forever(function () {
+    if ((knuffel || voeding) < 20) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        knuffel += 5
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        voeding += 5
+    }
+})
+basic.forever(function () {
+    if (dood == 1) {
+        basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+    }
 })
